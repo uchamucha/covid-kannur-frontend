249d8c6e80b69869dfcda6acf1f00874d5b53c12
Usamath PC <usamathpallikkachalil@gmail.com>
2020-08-31 02:10:55 +0530

    Updates

---
+++ precache-manifest.f637ca0bb8880b8734de9da360270af3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e23b540875fcd8c5e34e88be53640c",
+    "url": "/covid-kannur-frontend/index.html"
+  },
+  {
+    "revision": "3b533a7c602c894d7356",
+    "url": "/covid-kannur-frontend/static/css/main.d654dcae.chunk.css"
+  },
+  {
+    "revision": "aff590d75073ed3eea83",
+    "url": "/covid-kannur-frontend/static/js/2.3585ea91.chunk.js"
+  },
+  {
+    "revision": "e62aa1d43acfe02f0178e8e8fafda9bb",
+    "url": "/covid-kannur-frontend/static/js/2.3585ea91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b533a7c602c894d7356",
+    "url": "/covid-kannur-frontend/static/js/main.12713d61.chunk.js"
+  },
+  {
+    "revision": "7d4c5e25785e99dd3ce7",
+    "url": "/covid-kannur-frontend/static/js/runtime-main.a76070c9.js"
+  }
+]);
